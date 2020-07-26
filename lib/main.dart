2afa07af6b8280384eab04c912f37e84a0174e02
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-07-26 11:47:55 +0200

    ADded better translation method

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,49 @@
 import 'package:Remember/app/app.dart';
 import 'package:Remember/screens/menu.dart';
+import 'package:Remember/utils/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 void main() {
-  runApp(
-    App(
-      App.init(
-          title: Path("app.title"),
-          home: GameMenu(),
-          languages: {"de": "assets/de.json", "en": "assets/en.json"},
-          language: "de",
-          material: (context) => MaterialAppData(
-                theme: AppTheme.android(context),
-              ),
-          cupertino: (context) => CupertinoAppData(
-                theme: AppTheme.cupertino(context),
-              ),
-          isFullscreen: true),
-    ),
-  );
+  runApp(RememberApp());
+}
+
+class RememberApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Remember",
+      home: GameMenu(),
+         supportedLocales: [
+        Locale('en', 'US'),
+        Locale('de', 'DE'),
+        Locale('de', 'AT'),
+      ],
+      // These delegates make sure that the localization data for the proper language is loaded
+      localizationsDelegates: [
+        // THIS CLASS WILL BE ADDED LATER
+        // A class which loads the translations from JSON files
+        AppLocalizations.delegate,
+        // Built-in localization of basic text for Material widgets
+        GlobalMaterialLocalizations.delegate,
+        // Built-in localization for text direction LTR/RTL
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      // Returns a locale which will be used by the app
+      localeResolutionCallback: (locale, supportedLocales) {
+        // Check if the current device locale is supported
+        for (var supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale.languageCode &&
+              supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+        // If the locale of the device is not supported, use the first one
+        // from the list (English, in this case).
+        return supportedLocales.first;
+      },
+    );
+  }
 }
